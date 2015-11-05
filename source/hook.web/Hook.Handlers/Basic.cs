0318f1d4aacbd1304d31f857b67ed92d456e6de8
jbrinkman <joe@dnncorp.com>
2015-11-05 15:24:30 -0800

    Add changes for basic commit status update

--- source/hook.web/Hook.Handlers/Basic.cs
+++ source/hook.web/Hook.Handlers/Basic.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNet.WebHooks;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using System.Configuration;
+using Octokit;
 
 namespace CaptainHook.Web.Hook.Handlers
 {
@@ -21,7 +20,38 @@ namespace CaptainHook.Web.Hook.Handlers
 
             JObject data = context.GetDataOrDefault<JObject>();
 
-            return Task.FromResult(true);
+            return AssignPendingToCommit(data);
         }
+
+        /// <summary>
+        /// Setting GitHub Status using Octokit
+        /// </summary>
+        /// <returns>
+        ///     
+        /// </returns>
+        public async Task AssignPendingToCommit(JObject data)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("CaptainHook"));
+
+            var token = ConfigurationManager.AppSettings["github_token"];
+            if (token != null)
+            {
+                var tokenAuth = new Credentials(token);
+                client.Credentials = tokenAuth;
+
+                var status = new NewCommitStatus
+                {
+                    State = CommitState.Pending,
+                    Description = "This is a test status"
+                };
+
+                var repoOwner = data["repository"]["owner"]["login"].Value<string>();
+                var repoName = data["repository"]["name"].Value<string>();
+                var commitSha = data["pull_request"]["head"]["sha"].Value<string>();
+
+                var result = await client.Repository.CommitStatus.Create(repoOwner, repoName, commitSha, status);
+            }
+        }
+
     }
 }
