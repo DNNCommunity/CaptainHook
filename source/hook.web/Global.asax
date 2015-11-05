adaaf2228cf9ce1fbe71a61b608438c19f8c8183
jbrinkman <joe@dnncorp.com>
2015-11-05 10:36:00 -0800

    Basic framework - Fix #1 This includes a basic hook that we'll use as the foundation.

---
+++ source/hook.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CaptainHook.Web.Global" Language="C#" %>
---
+++ source/hook.web/Hook.Handlers/Basic.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.WebHooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CaptainHook.Web.Hook.Handlers
+{
+    public class Basic : WebHookHandler
+    {
+        public Basic()
+        {
+            this.Receiver = "github";
+        }
+
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            string action = context.Actions.First();
+
+            JObject data = context.GetDataOrDefault<JObject>();
+
+            return Task.FromResult(true);
+        }
+    }
+}
